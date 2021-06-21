632b1af3150a0cfaae4e6ce90d39cb7896a029a2
John Yevsukov <github.com@john.engineering>
2021-06-21 00:30:53 -0400

    more route setup

--- api/server.js
+++ api/server.js
@@ -2,9 +2,21 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const authRouter = require('./auth/auth-router');
+
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+
+server.use('/api/auth', authRouter);
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      custom: 'something went wrong in the api',
+      message: err.message,
+      stack: err.stack,
+    })
+  })
 
 module.exports = server
